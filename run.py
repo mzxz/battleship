b00b65b8bbf0378e301c8d4fd9d9aa14f92fc740
mzxz <mohamed.abaker110@gmail.com>
2021-11-17 09:07:01 +0000

    Initial The program main structure

---
+++ run.py
@@ -0,0 +1,21 @@
+from random import randint
+
+
+class Battelship:
+    pass
+
+
+def print_a(board):
+    pass
+
+
+def random_row(board):
+
+    pass
+
+
+def random_col(board):
+
+
+def user_guesses(board):
+    pass
