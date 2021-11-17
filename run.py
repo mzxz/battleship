554d9569d40d5a8a8a468a6c7e14b63f1bee95ac
mzxz <mohamed.abaker110@gmail.com>
2021-11-17 09:53:14 +0000

    add user guess function and fix indentions

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ class Battelship:
 
 player_name = Battelship()
 player_name = input("Please enter your name : \n")
-print(f"welcome To Battelship Game{player_name}")
+print(f"welcome To Battelship Game:\n {player_name}")
 print('='*22)
 
 
@@ -48,4 +48,51 @@ def random_col(board):
 
 
 def user_guesses(board):
-    pass
+    """
+    Main function for user gesses, and cheack game in all situations
+    """
+    ship_row = random_row(board)
+    ship_col = random_col(board)
+    user_guesses = 0
+    while True:
+        print("=" * 22)
+
+        try:
+            guess_ship_row = int(input('Guess ship Row: '))
+            guess_ship_col = int(input('Guess ship Col: '))
+        except ValueError as e:
+            print(f"Invalid data: {e}, please try again.\n")
+            return False
+
+        if guess_ship_row == ship_row and guess_ship_col == ship_col:
+            board[ship_row][ship_col] = '*'
+
+            print('Congoratulation! You sank The battleship!')
+            print_a(board)
+            break
+
+        elif guess_ship_row not in range(5) or guess_ship_col not in range(5):
+            print("That's out of range.")
+
+        elif board[guess_ship_row][guess_ship_col] == 'X':
+            print("You guessed that one already.")
+
+        else:
+            print('You missed my battelship')
+            board[guess_ship_row][guess_ship_col] = 'X'
+            print_a(board)
+
+            user_guesses = user_guesses + 1
+            print(f"It's your turn: {user_guesses}")
+
+            if user_guesses == 4:
+                print_a(board)
+                print("Game is over! You have run out of turns!")
+                print(ship_row)
+                print(ship_col)
+                break
+
+
+user_guesses(board)
+
+
